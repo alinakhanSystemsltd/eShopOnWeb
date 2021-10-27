1d8c7fd7ede9db08dbbe81a97a34d35faded4704
alinakhanSystemsltd <90306505+alinakhanSystemsltd@users.noreply.github.com>
2021-10-28 01:31:49 +0500

    Update dotnet.ps1

--- scripts/dotnet.ps1
+++ scripts/dotnet.ps1
@@ -2,3 +2,4 @@ tar -xvf C:/DotNetFolder/dotnetapp.tar.gz -C C:/DotNetFolder
 dotnet restore C:/DotNetFolder/eShopOnWeb.sln
 dotnet build eShopOnWeb.sln  --configuration Release
 dotnet add C:/DotNetFolder/tests/UnitTests/UnitTests.csproj package JUnitTestLogger --version 1.1.0
+dotnet test C:/DotNetFolder/tests/UnitTests/UnitTests.csproj --logger \"junit;LogFilePath=\"C:/DotNetFolder\"/TestResults/1.0.0.\"37\"/results.xml\" --configuration release --collect \"Code coverage\" /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutput='./TestResults/' /p:Threshold=80
