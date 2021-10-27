3a9bc57153d727a16a7a882e110dee2c6f4e64d7
alinakhanSystemsltd <90306505+alinakhanSystemsltd@users.noreply.github.com>
2021-10-28 01:52:57 +0500

    Update dotnet.ps1

--- scripts/dotnet.ps1
+++ scripts/dotnet.ps1
@@ -2,4 +2,4 @@ tar -xvf C:/DotNetFolder/dotnetapp.tar.gz -C C:/DotNetFolder
 dotnet restore C:/DotNetFolder/eShopOnWeb.sln
 dotnet build eShopOnWeb.sln  --configuration Release
 dotnet add C:/DotNetFolder/tests/UnitTests/UnitTests.csproj package JUnitTestLogger --version 1.1.0
-dotnet test C:/DotNetFolder/tests/UnitTests/UnitTests.csproj 
+dotnet test C:/DotNetFolder/tests/UnitTests/UnitTests.csproj  --logger:"trx;logFileName=unitReports.xml" --collect "Code coverage" /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput="./TestResults/" --results-directory BuildReports
