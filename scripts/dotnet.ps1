ec2f36d861027e5bde02e0473aeceeeee12a0dfd
alinakhanSystemsltd <90306505+alinakhanSystemsltd@users.noreply.github.com>
2021-10-28 01:26:51 +0500

    Update dotnet.ps1

--- scripts/dotnet.ps1
+++ scripts/dotnet.ps1
@@ -1,3 +1,4 @@
 tar -xvf C:/DotNetFolder/dotnetapp.tar.gz -C C:/DotNetFolder
 dotnet restore C:/DotNetFolder/eShopOnWeb.sln
 dotnet build eShopOnWeb.sln  --configuration Release
+dotnet add C:/DotNetFolder/eShopOnWeb.sln/tests/UnitTests/UnitTests.csproj package JUnitTestLogger --version 1.1.0
