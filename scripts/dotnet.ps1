831b47c5105b2b202dd62eb7b7b5fdd7d13cbd8d
alinakhanSystemsltd <90306505+alinakhanSystemsltd@users.noreply.github.com>
2021-10-28 11:51:30 +0500

    Update dotnet.ps1

--- scripts/dotnet.ps1
+++ scripts/dotnet.ps1
@@ -1,5 +1,17 @@
 tar -xvf C:/DotNetFolder/dotnetapp.tar.gz -C C:/DotNetFolder
 dotnet restore C:/DotNetFolder/eShopOnWeb.sln
 dotnet build eShopOnWeb.sln  --configuration Release
-dotnet add C:/DotNetFolder/tests/UnitTests/UnitTests.csproj package JUnitTestLogger --version 1.1.0
-dotnet test C:/DotNetFolder/tests/UnitTests/UnitTests.csproj  --logger:"trx;logFileName=unitReports.xml" --collect "Code coverage" /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput="./TestResults/" --results-directory BuildReports
+dotnet test C:/DotNetFolder/tests/UnitTests/UnitTests.csproj dotnet test --logger "trx;LogFileName=TestResults.trx" ^
+            --logger "xunit;LogFileName=TestResults.xml" ^
+            --results-directory ./BuildReports/UnitTests ^
+            /p:CollectCoverage=true ^
+            /p:CoverletOutput=BuildReports\Coverage\ ^
+            /p:CoverletOutputFormat=cobertura ^
+            /p:Exclude="[xunit.*]
+# $destinationFolder = \"$env:WORKSPACE/TestResults\"
+# if (!(Test-Path -path $destinationFolder)) {New-Item $destinationFolder -Type Directory}
+# $file = Get-ChildItem -Path \"$env:WORKSPACE/tests\\UnitTests/TestResults/*/*.coverage\"
+# $file | Rename-Item -NewName testcoverage.coverage
+# $renamedFile = Get-ChildItem -Path \"$env:WORKSPACE/tests\\UnitTests/TestResults/*/*.coverage\"
+# Copy-Item $renamedFile -Destination $destinationFolder
+                    
