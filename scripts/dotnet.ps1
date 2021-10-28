6c20d9b58bc580d3a04dda41de5fc863f6383b85
alinakhanSystemsltd <90306505+alinakhanSystemsltd@users.noreply.github.com>
2021-10-28 12:02:12 +0500

    Update dotnet.ps1

--- scripts/dotnet.ps1
+++ scripts/dotnet.ps1
@@ -1,6 +1,6 @@
 tar -xvf C:/DotNetFolder/dotnetapp.tar.gz -C C:/DotNetFolder
 dotnet restore C:/DotNetFolder/eShopOnWeb.sln
 dotnet build eShopOnWeb.sln  --configuration Release
-dotnet test C:/DotNetFolder/tests/UnitTests/UnitTests.csproj --logger "trx;LogFileName=TestResults.trx" --logger "xunit;LogFileName=TestResults.xml" --results-directory ./BuildReports/UnitTests /p:CollectCoverage=true /p:CoverletOutput=BuildReports\Coverage\ /p:CoverletOutputFormat=cobertura /p:Exclude="[xunit.*]"
-
-                    
+dotnet test eShopOnWeb.sln -c Release --no-build /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura
+reportgenerator -reports:*/*/coverage.cobertura.xml -targetdir:CodeCoverageResult -reporttypes:Cobertura
+publishCoverage adapters: [coberturaAdapter('CodeCoverageResult/Cobertura.xml')]             
