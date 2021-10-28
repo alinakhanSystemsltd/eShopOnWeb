48748e0869b388c54810362fb7fd083ca300c7d8
alinakhanSystemsltd <90306505+alinakhanSystemsltd@users.noreply.github.com>
2021-10-28 11:53:23 +0500

    Update dotnet.ps1

--- scripts/dotnet.ps1
+++ scripts/dotnet.ps1
@@ -1,13 +1,7 @@
 tar -xvf C:/DotNetFolder/dotnetapp.tar.gz -C C:/DotNetFolder
 dotnet restore C:/DotNetFolder/eShopOnWeb.sln
 dotnet build eShopOnWeb.sln  --configuration Release
-dotnet test C:/DotNetFolder/tests/UnitTests/UnitTests.csproj dotnet test --logger "trx;LogFileName=TestResults.trx" ^
-            --logger "xunit;LogFileName=TestResults.xml" ^
-            --results-directory ./BuildReports/UnitTests ^
-            /p:CollectCoverage=true ^
-            /p:CoverletOutput=BuildReports\Coverage\ ^
-            /p:CoverletOutputFormat=cobertura ^
-            /p:Exclude="[xunit.*]
+dotnet test C:/DotNetFolder/tests/UnitTests/UnitTests.csproj dotnet test --logger "trx;LogFileName=TestResults.trx" --logger "xunit;LogFileName=TestResults.xml" --results-directory ./BuildReports/UnitTests /p:CollectCoverage=true /p:CoverletOutput=BuildReports\Coverage\ /p:CoverletOutputFormat=cobertura /p:Exclude="[xunit.*]
 # $destinationFolder = \"$env:WORKSPACE/TestResults\"
 # if (!(Test-Path -path $destinationFolder)) {New-Item $destinationFolder -Type Directory}
 # $file = Get-ChildItem -Path \"$env:WORKSPACE/tests\\UnitTests/TestResults/*/*.coverage\"
