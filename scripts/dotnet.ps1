290400ec42af99d70efa53f4a31763682260dd65
alinakhanSystemsltd <90306505+alinakhanSystemsltd@users.noreply.github.com>
2021-10-28 11:54:42 +0500

    Update dotnet.ps1

--- scripts/dotnet.ps1
+++ scripts/dotnet.ps1
@@ -2,10 +2,5 @@ tar -xvf C:/DotNetFolder/dotnetapp.tar.gz -C C:/DotNetFolder
 dotnet restore C:/DotNetFolder/eShopOnWeb.sln
 dotnet build eShopOnWeb.sln  --configuration Release
 dotnet test C:/DotNetFolder/tests/UnitTests/UnitTests.csproj dotnet test --logger "trx;LogFileName=TestResults.trx" --logger "xunit;LogFileName=TestResults.xml" --results-directory ./BuildReports/UnitTests /p:CollectCoverage=true /p:CoverletOutput=BuildReports\Coverage\ /p:CoverletOutputFormat=cobertura /p:Exclude="[xunit.*]
-# $destinationFolder = \"$env:WORKSPACE/TestResults\"
-# if (!(Test-Path -path $destinationFolder)) {New-Item $destinationFolder -Type Directory}
-# $file = Get-ChildItem -Path \"$env:WORKSPACE/tests\\UnitTests/TestResults/*/*.coverage\"
-# $file | Rename-Item -NewName testcoverage.coverage
-# $renamedFile = Get-ChildItem -Path \"$env:WORKSPACE/tests\\UnitTests/TestResults/*/*.coverage\"
-# Copy-Item $renamedFile -Destination $destinationFolder
+
                     
