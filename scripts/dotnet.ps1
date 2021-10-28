b5a89537a164a21dcd3a021510be670399f3dd22
alinakhanSystemsltd <90306505+alinakhanSystemsltd@users.noreply.github.com>
2021-10-28 12:17:14 +0500

    Update dotnet.ps1

--- scripts/dotnet.ps1
+++ scripts/dotnet.ps1
@@ -1,6 +1,6 @@
 tar -xvf C:/DotNetFolder/dotnetapp.tar.gz -C C:/DotNetFolder
+Install-Package coverlet.msbuild -Version 3.1.0 
 dotnet restore C:/DotNetFolder/eShopOnWeb.sln
 dotnet build eShopOnWeb.sln  --configuration Release
-dotnet test eShopOnWeb.sln -c Release --no-build /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura
-C:/ReportGenerator/net5.0/ReportGenerator.exe -reports:*/*/coverage.cobertura.xml -targetdir:CodeCoverageResult -reporttypes:Cobertura
-publishCoverage adapters: [coberturaAdapter('CodeCoverageResult/Cobertura.xml')]             
+dotnet test --logger "trx;LogFileName=TestResults.trx" --logger "xunit;LogFileName=TestResults.xml" --results-directory ./BuildReports/UnitTests /p:CollectCoverage=true /p:CoverletOutput=BuildReports\Coverage\ /p:CoverletOutputFormat=cobertura /p:Exclude="[xunit.*]*
+dotnet C:/ReportGenerator/net5.0/ReportGenerator.exe "-reports:BuildReports\Coverage\coverage.cobertura.xml" "-targetdir:BuildReports\Coverage" -reporttypes:HTML;HTMLSummary
