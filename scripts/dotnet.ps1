2cb71b9dc771bae5cbfd3ca2d31eac5544e99baf
alinakhanSystemsltd <90306505+alinakhanSystemsltd@users.noreply.github.com>
2021-10-28 12:05:01 +0500

    Update dotnet.ps1

--- scripts/dotnet.ps1
+++ scripts/dotnet.ps1
@@ -2,5 +2,5 @@ tar -xvf C:/DotNetFolder/dotnetapp.tar.gz -C C:/DotNetFolder
 dotnet restore C:/DotNetFolder/eShopOnWeb.sln
 dotnet build eShopOnWeb.sln  --configuration Release
 dotnet test eShopOnWeb.sln -c Release --no-build /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura
-reportgenerator -reports:*/*/coverage.cobertura.xml -targetdir:CodeCoverageResult -reporttypes:Cobertura
+C:/ReportGenerator/net5.0/ReportGenerator.exe -reports:*/*/coverage.cobertura.xml -targetdir:CodeCoverageResult -reporttypes:Cobertura
 publishCoverage adapters: [coberturaAdapter('CodeCoverageResult/Cobertura.xml')]             
