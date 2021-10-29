265087a7398876e05870bde0ed23410717f88071
alinakhanSystemsltd <90306505+alinakhanSystemsltd@users.noreply.github.com>
2021-10-29 23:40:30 +0500

    Update dotnet.ps1

--- scripts/dotnet.ps1
+++ scripts/dotnet.ps1
@@ -1,4 +1,4 @@
-tar -xvf C:/DotNetFolder2/dotnetapp.tar.gz -C C:/DotNetFolder2
-dotnet restore C:/DotNetFolder2/eShopOnWeb.sln
+tar -xvf C:/DotNetFolder/dotnetapp.tar.gz -C C:/DotNetFolder2
+dotnet restore C:/DotNetFolder/eShopOnWeb.sln
 dotnet build eShopOnWeb.sln  --configuration Release
 dotnet publish -c Release -r win-x64 --output ./DotNetFolder eShopOnWeb.sln
